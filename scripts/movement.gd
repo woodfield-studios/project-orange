5b8a18c1ec4bcd6242b2e7c417907b2cde168690
Chriss <drago15243@gmail.com>
2026-03-25 23:16:53 -0500

    Player Movement Initial Fix
    Movement not correct but moving.

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff84fb31e1aa4cafe1774aacabbb5667254c6ec1b292f49343863345d5153ed8
-size 882
+oid sha256:598c018207ea76c4cbe9b2055e334200aef1eacc56e9299bd7c09f13fd7e8338
+size 966
