81a902732eb1d9baf8512b846fd7c75bf372b845
Chriss <drago15243@gmail.com>
2026-03-26 21:12:25 -0500

    Player Movement Fix
    Movement now towards the direction you look.

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export_group("Dependencies")
 @export var character_body: CharacterBody3D
+@export var rotation_reference: Node3D
 
 @export_group("Options")
 @export var speed : float = 25.0
@@ -9,13 +10,16 @@ extends Node
 @export var jump_velocity : float = -500.0
 @export var gravity : float = -9.8
 
-func _physics_process(_delta):
-	#character_body.velocity.y = move_toward(character_body.velocity.y, gravity, gravity)
+func _physics_process(delta):
+	character_body.velocity.y = move_toward(character_body.velocity.y, gravity, delta)
 	
 	var direction2: Vector2 = Input.get_vector("left", "right", "forward", "backward").normalized()
 	# Need to take into account camera direction to calculate proper forward direction
 	var direction = Vector3(direction2.x, 0, direction2.y)
 	var current_speed = speed if !Input.is_action_pressed("sprint") else (speed * sprint_multiplier)
+	
+	var camera_transform = rotation_reference.transform.orthonormalized()
+	direction = camera_transform * Vector3(direction2.x, 0, direction2.y)
 	
 	if direction != Vector3.ZERO:
 		character_body.velocity = direction * current_speed
