1b4e01fbaf3153b30e2152e7cf9e735f8b6a8031
Chriss <drago15243@gmail.com>
2026-03-26 21:12:25 -0500

    Player Movement Fix
    Movement now towards the direction you look.

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:598c018207ea76c4cbe9b2055e334200aef1eacc56e9299bd7c09f13fd7e8338
-size 966
+oid sha256:d35a87a60a75c9b06cbdd893e383e7c4f8c1be7871008b84e69435d9c3a61cb0
+size 1145
