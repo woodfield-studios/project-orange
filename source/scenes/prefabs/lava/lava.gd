bb2d2cce5cd6c4b95a46a6273177d6bb6054c838
Jet <jetgeronimo@gmail.com>
2026-06-21 16:26:39 -0500

    Make lava damage an exported variable

--- source/scenes/prefabs/lava/lava.gd
+++ source/scenes/prefabs/lava/lava.gd
@@ -1,7 +1,10 @@
 extends StaticBody3D
 
+@export var damage: int = 1
+
+
 func interact(collider: PhysicsBody3D):
 	var health_component = collider.get_node_or_null("HealthComponent")
 
 	if health_component and health_component.has_method("take_damage"):
-		health_component.take_damage(1, self)
+		health_component.take_damage(damage, self)
