7cd64fe29fa547fe0d6333eb84025d4ee49ac185
Jet <jetgeronimo@gmail.com>
2026-06-21 16:29:21 -0500

    Add underscore to unused delta parameter

--- source/entities/player/character_body_3d.gd
+++ source/entities/player/character_body_3d.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
