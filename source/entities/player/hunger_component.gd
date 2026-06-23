c67a4fbd82f9744051fadd44e13e85c6abe22d3d
Jet <jetgeronimo@gmail.com>
2026-06-23 00:01:18 -0500

    Implement hunger timer

--- source/entities/player/hunger_component.gd
+++ source/entities/player/hunger_component.gd
@@ -1,11 +1,23 @@
+class_name HungerComponent
 extends Node
 
+signal hunger_changed(new_hunger: int, max_hunger: int)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var max_hunger: int = 100
+@export var hunger_rate: float = 0.1
+
+var current_hunger: int = max_hunger
+var time_elapsed: float = 0.0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	time_elapsed += delta
+
+	if time_elapsed >= 1.0 / hunger_rate:
+		tick_hunger()
+		time_elapsed = 0.0
+
+
+func tick_hunger() -> void:
+	current_hunger -= 1
+	hunger_changed.emit(current_hunger, max_hunger)
