12ec8ab95e566617e2fddd29bc06aa6f4ee85c0b
Jet <jetgeronimo@gmail.com>
2026-06-23 00:20:31 -0500

    Press 'e' to sate hunger

--- source/entities/player/hunger_component.gd
+++ source/entities/player/hunger_component.gd
@@ -12,6 +12,9 @@ var time_elapsed: float = 0.0
 
 
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("eat"):
+		sate(10)
+
 	time_elapsed += delta
 
 	if time_elapsed >= 1.0 / hunger_rate:
@@ -25,3 +28,7 @@ func tick_hunger() -> void:
 	else:
 		current_hunger = max(0, current_hunger - 1)
 		hunger_changed.emit(current_hunger, max_hunger)
+
+
+func sate(amount: int) -> void:
+	current_hunger = min(max_hunger, current_hunger + amount)
