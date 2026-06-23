3095332611c0dc80834c0110062a1a35b9898952
Jet <jetgeronimo@gmail.com>
2026-06-23 00:35:17 -0500

    Rename `tick_hunger` to `tick_starve`

--- source/entities/player/hunger_component.gd
+++ source/entities/player/hunger_component.gd
@@ -2,7 +2,7 @@ class_name HungerComponent
 extends Node
 
 signal hunger_changed(new_hunger: int, max_hunger: int)
-signal tick_hunger_damage()
+signal tick_starve()
 
 @export var max_hunger: int = 100
 @export var hunger_rate: float = 0.1
@@ -24,7 +24,7 @@ func _process(delta: float) -> void:
 
 func tick_hunger() -> void:
 	if current_hunger <= 0:
-		tick_hunger_damage.emit()
+		tick_starve.emit()
 	else:
 		current_hunger = max(0, current_hunger - 1)
 		hunger_changed.emit(current_hunger, max_hunger)
