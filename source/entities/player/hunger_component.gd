fe6abe61bb58647dfce67bcba10fc4f2aaf93404
Jet <jetgeronimo@gmail.com>
2026-06-28 11:20:02 -0500

    Make hunger deal damage to player

--- source/entities/player/hunger_component.gd
+++ source/entities/player/hunger_component.gd
@@ -2,10 +2,11 @@ class_name HungerComponent
 extends Node
 
 signal hunger_changed(new_hunger: int, max_hunger: int)
-signal tick_starve()
+signal tick_starve(starve_damage: int)
 
 @export var max_hunger: int = 100
 @export var hunger_rate: float = 0.1
+@export var starve_damage: int = 1
 
 var current_hunger: int = max_hunger
 var time_elapsed: float = 0.0
@@ -24,7 +25,7 @@ func _process(delta: float) -> void:
 
 func tick_hunger() -> void:
 	if current_hunger <= 0:
-		tick_starve.emit()
+		tick_starve.emit(starve_damage)
 	else:
 		current_hunger = max(0, current_hunger - 1)
 		hunger_changed.emit(current_hunger, max_hunger)
