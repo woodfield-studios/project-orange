f35364ed10f47c5983761d1f2555c7a8c384c1ce
Jet <jetgeronimo@gmail.com>
2026-06-28 11:29:20 -0500

    [PRO-25] Implement hunger (#2)
    * Create hunger component
    
    * Implement hunger timer
    
    * Attach hunger component to player
    
    * Implement hunger damage ticks
    
    * Press 'e' to sate hunger
    
    * Rename `tick_hunger` to `tick_starve`
    
    * Make hunger deal damage to player
    
    * Add hunger to HUD

--- source/ui/hud/hud.gd
+++ source/ui/hud/hud.gd
@@ -1,4 +1,8 @@
 extends Control
 
 func _on_health_component_health_changed(new_health: int, max_health: int) -> void:
-	$Health.text = str(new_health, "/", max_health)
+	get_node("Health").text = str(new_health, "/", max_health)
+
+
+func _on_hunger_component_hunger_changed(new_hunger: int, max_hunger: int) -> void:
+	get_node("Hunger").text = str(new_hunger, "/", max_hunger)
