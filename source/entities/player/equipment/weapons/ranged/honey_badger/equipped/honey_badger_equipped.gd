69bacac4aaa916aa6ec2d14c112a05e8b7aa059f
Jet <jetgeronimo@gmail.com>
2026-09-10 22:05:02 -0500

    Implement bullet holes

--- source/entities/player/equipment/weapons/ranged/honey_badger/equipped/honey_badger_equipped.gd
+++ source/entities/player/equipment/weapons/ranged/honey_badger/equipped/honey_badger_equipped.gd
@@ -1,9 +1,15 @@
 extends Node3D
 
 @export var raycast: RayCast3D
+@export var bullet_hole_scene: PackedScene
+
 
 func use() -> void:
 	var collider: Node3D = raycast.get_collider()
 	if collider:
 		var collision_normal: Vector3 = raycast.get_collision_normal()
-		var collision_pointer: Vector3 = raycast.get_collision_point()
+		var collision_point: Vector3 = raycast.get_collision_point()
+		var bullet_hole: Node3D = bullet_hole_scene.instantiate()
+		var orientation_point: Vector3 = collision_point + collision_normal
+		bullet_hole.look_at_from_position(collision_point, orientation_point)
+		collider.add_child(bullet_hole)
