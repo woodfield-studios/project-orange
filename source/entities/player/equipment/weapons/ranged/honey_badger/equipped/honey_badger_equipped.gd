cfe75d0e6092e616d8dd0ab6ed4a3c2d95cc6056
Jet <jetgeronimo@gmail.com>
2026-09-12 22:23:11 -0500

    Break creating bullet holes into its own private method

--- source/entities/player/equipment/weapons/ranged/honey_badger/equipped/honey_badger_equipped.gd
+++ source/entities/player/equipment/weapons/ranged/honey_badger/equipped/honey_badger_equipped.gd
@@ -8,18 +8,7 @@ extends Node3D
 func use() -> void:
 	var collider: Node3D = raycast.get_collider()
 	if collider:
-		var collision_normal: Vector3 = raycast.get_collision_normal()
-		var collision_point: Vector3 = raycast.get_collision_point()
-		var bullet_hole: Node3D = bullet_hole_scene.instantiate()
-
-		collider.add_child(bullet_hole)
-
-		bullet_hole.global_position = collision_point
-		if collision_normal != Vector3.UP:
-			var orientation_point: Vector3 = collision_point + collision_normal
-			bullet_hole.look_at(orientation_point)
-		else:
-			bullet_hole.rotate_x(-90.0)
+		_create_bullet_hole(collider)
 
 		var health_component: HealthComponent = collider.get_node_or_null("HealthComponent")
 		if health_component:
@@ -27,3 +16,17 @@ func use() -> void:
 
 	honey_badger.play_audio()
 	honey_badger.play_animation()
+
+func _create_bullet_hole(collider: Node3D) -> void:
+	var collision_normal: Vector3 = raycast.get_collision_normal()
+	var collision_point: Vector3 = raycast.get_collision_point()
+	var bullet_hole: Node3D = bullet_hole_scene.instantiate()
+
+	collider.add_child(bullet_hole)
+
+	bullet_hole.global_position = collision_point
+	if collision_normal != Vector3.UP:
+		var orientation_point: Vector3 = collision_point + collision_normal
+		bullet_hole.look_at(orientation_point)
+	else:
+		bullet_hole.rotate_x(-90.0)
